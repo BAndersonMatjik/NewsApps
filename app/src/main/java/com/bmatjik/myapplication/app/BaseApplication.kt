41b6cec1cb5ab67ec483c9bf5a28e791f3ac6e21
billyandersonmatjik <billy.matjik@anteraja.id>
2023-08-03 13:47:32 +0700

    Initial commit

---
+++ app/src/main/java/com/bmatjik/myapplication/app/BaseApplication.kt
@@ -0,0 +1,18 @@
+package com.bmatjik.myapplication.app
+
+import android.app.Application
+import android.content.pm.ApplicationInfo
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class BaseApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (applicationInfo.flags==ApplicationInfo.FLAG_DEBUGGABLE){
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+}
