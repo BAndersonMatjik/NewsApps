cc68584e98b5bbcd4944aa9ea9632551630b59a0
billyandersonmatjik <billy.matjik@anteraja.id>
2023-08-03 15:09:39 +0700

    Add Qualifier and Module and Provider

---
+++ app/src/main/java/com/bmatjik/myapplication/core/di/CoreModule.kt
@@ -0,0 +1,56 @@
+package com.bmatjik.myapplication.core.di
+
+import com.bmatjik.myapplication.BuildConfig
+import com.bmatjik.myapplication.MainActivity
+import com.bmatjik.myapplication.core.remote.api.NewsApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object CoreModule {
+
+    @Provides
+    @NewsUrl
+    @Singleton
+    fun providesNewsApiUrl():String = BuildConfig.NEWS_API_URL
+
+    @Provides
+    @NewsKey
+    @Singleton
+    fun providesNewsApiKey():String = BuildConfig.NEWS_API_KEY
+
+    @Provides
+    @Singleton
+    fun providesRetrofit(@NewsUrl url: String):Retrofit{
+        return Retrofit.Builder().baseUrl(url)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    @InstallIn(SingletonComponent::class)
+    @Module
+    object CoreModule{
+        @Singleton
+        @Provides
+        fun providesGeoApi(retrofit: Retrofit):NewsApi{
+            return retrofit.create(NewsApi::class.java)
+        }
+    }
+
+
+    //handle qualifier for DI
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class NewsUrl
+
+    @Qualifier
+    @Retention(AnnotationRetention.BINARY)
+    annotation class NewsKey
+}
