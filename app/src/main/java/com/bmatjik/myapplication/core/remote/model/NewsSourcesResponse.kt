14751eb0ec3a1bd9196e429f9b2d37fed37a0491
billyandersonmatjik <billy.matjik@anteraja.id>
2023-08-05 06:25:42 +0700

    Add @Json for support moshi json converter

--- app/src/main/java/com/bmatjik/myapplication/core/remote/model/NewsSourcesResponse.kt
+++ app/src/main/java/com/bmatjik/myapplication/core/remote/model/NewsSourcesResponse.kt
@@ -1,9 +1,15 @@
 package com.bmatjik.myapplication.core.remote.model
 
- data class NewsSourcesResponse(
+import com.squareup.moshi.Json
+
+data class NewsSourcesResponse(
+     @Json(name = "status")
     val status: String,
+     @Json(name = "code")
     val code:String?="",
+     @Json(name = "message")
     val message:String?="",
+     @Json(name = "sources")
     val sources:List<NewsSourceResponse>?=null
 )
 
