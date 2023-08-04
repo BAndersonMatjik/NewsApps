34ab13b0963948248ba6f019dcbc1a1a929bd900
billyandersonmatjik <billy.matjik@anteraja.id>
2023-08-05 02:13:51 +0700

    Add NewsCategory

--- app/src/main/java/com/bmatjik/myapplication/view/MovieListCategoryFragment.kt
+++ app/src/main/java/com/bmatjik/myapplication/view/MovieListCategoryFragment.kt
@@ -1,27 +1,37 @@
 package com.bmatjik.myapplication.view
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+
 import android.view.ViewGroup
-import androidx.viewbinding.ViewBinding
-import com.bmatjik.myapplication.R
+import androidx.fragment.app.viewModels
+import com.bmatjik.myapplication.common.BaseFragment
+import com.bmatjik.myapplication.common.Constants
+import com.bmatjik.myapplication.common.MarginItemDecoration
 import com.bmatjik.myapplication.databinding.FragmentMovieListCategoryBinding
+import com.bmatjik.myapplication.view.adapter.CategoryAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MovieListCategoryFragment : BaseFragment<FragmentMovieListCategoryBinding>() {
 
-    private lateinit var viewModel: MovieListCategoryViewModel
-
+    private val adapter by lazy {
+        CategoryAdapter()
+    }
     override fun setupViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentMovieListCategoryBinding = FragmentMovieListCategoryBinding.inflate(inflater,container,false)
 
     override fun initView() {
-        binding.tvTest.text = "Test"
+        binding.rvCategory.adapter = adapter
+        binding.rvCategory.addItemDecoration(MarginItemDecoration(6))
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        adapter.submit(Constants.NewsCategory)
     }
 
 }
