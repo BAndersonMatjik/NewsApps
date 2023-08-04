8d42b0f7994b78e2ca66a7bca4287a246663579d
billyandersonmatjik <billy.matjik@anteraja.id>
2023-08-05 06:30:43 +0700

    Add new constant url to get flag image

--- app/src/main/java/com/bmatjik/myapplication/common/Constants.kt
+++ app/src/main/java/com/bmatjik/myapplication/common/Constants.kt
@@ -2,4 +2,6 @@ package com.bmatjik.myapplication.common
 
 object Constants {
     val NewsCategory = listOf("business","general","health","science","sport","technology")
+    val FlagUrl = "https://flagcdn.com/w80/#img.png"
+
 }
