336241f2601f8b8ae89b447069fb1369ebd45d37
billyandersonmatjik <billy.matjik@anteraja.id>
2023-08-05 00:05:48 +0700

    Move to New Package View

---
+++ app/src/main/java/com/bmatjik/myapplication/view/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.bmatjik.myapplication.view
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.bmatjik.myapplication.core.di.CoreModule
+import com.bmatjik.myapplication.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainActivity :AppCompatActivity() {
+
+    @Inject
+    @CoreModule.NewsUrl
+    internal lateinit var url :String
+
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Toast.makeText(this, "$url", Toast.LENGTH_SHORT).show()
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+}
