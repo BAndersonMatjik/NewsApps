33c89133f99bdfb7bbca204870d6930c115d7fb8
billyandersonmatjik <billy.matjik@anteraja.id>
2023-08-05 06:28:28 +0700

    add action when click category to Move to Another Fragment

--- app/src/main/java/com/bmatjik/myapplication/view/NewsListCategoryFragment.kt
+++ app/src/main/java/com/bmatjik/myapplication/view/NewsListCategoryFragment.kt
@@ -5,16 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.bmatjik.myapplication.common.BaseFragment
 import com.bmatjik.myapplication.common.Constants
 import com.bmatjik.myapplication.common.MarginItemDecoration
-import com.bmatjik.myapplication.databinding.FragmentMovieListCategoryBinding
+import com.bmatjik.myapplication.databinding.FragmentNewsListCategoryBinding
 import com.bmatjik.myapplication.view.adapter.CategoryAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MovieListCategoryFragment : BaseFragment<FragmentMovieListCategoryBinding>() {
+class NewsListCategoryFragment : BaseFragment<FragmentNewsListCategoryBinding>() {
 
     private val adapter by lazy {
         CategoryAdapter()
@@ -22,7 +23,7 @@ class MovieListCategoryFragment : BaseFragment<FragmentMovieListCategoryBinding>
     override fun setupViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
-    ): FragmentMovieListCategoryBinding = FragmentMovieListCategoryBinding.inflate(inflater,container,false)
+    ): FragmentNewsListCategoryBinding = FragmentNewsListCategoryBinding.inflate(inflater,container,false)
 
     override fun initView() {
         binding.rvCategory.adapter = adapter
@@ -31,6 +32,11 @@ class MovieListCategoryFragment : BaseFragment<FragmentMovieListCategoryBinding>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        adapter.setListenerItem(object : CategoryAdapter.OnClickItem{
+            override fun onClickItem(item: String) {
+                findNavController().navigate(NewsListCategoryFragmentDirections.actionNewsListCategoryFragmentToNewsSourceFragment())
+            }
+        })
         adapter.submit(Constants.NewsCategory)
     }
 
