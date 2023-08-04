569009005aefcc00d1de0d51a8aa8d0e2cd5917b
billyandersonmatjik <billy.matjik@anteraja.id>
2023-08-05 06:58:11 +0700

    Optimized Import Code

--- app/src/main/java/com/bmatjik/myapplication/app/BaseApplication.kt
+++ app/src/main/java/com/bmatjik/myapplication/app/BaseApplication.kt
@@ -1,7 +1,6 @@
 package com.bmatjik.myapplication.app
 
 import android.app.Application
-import android.content.pm.ApplicationInfo
 import com.bmatjik.myapplication.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
