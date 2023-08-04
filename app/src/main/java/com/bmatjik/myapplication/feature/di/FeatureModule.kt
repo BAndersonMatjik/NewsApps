e5ae4eda040e63bef1b19b9024c8fcdeaa56ba84
billyandersonmatjik <billy.matjik@anteraja.id>
2023-08-05 06:29:40 +0700

    Register Usecase at dagger module

---
+++ app/src/main/java/com/bmatjik/myapplication/feature/di/FeatureModule.kt
@@ -0,0 +1,27 @@
+package com.bmatjik.myapplication.feature.di
+
+import com.bmatjik.myapplication.feature.usecase.GetCategoryUseCase
+import com.bmatjik.myapplication.feature.usecase.impl.GetCategoryUsecaseImpl
+import com.bmatjik.myapplication.feature.usecase.GetNewsSourcesUsecase
+import com.bmatjik.myapplication.feature.usecase.impl.GetNewsSourcesUsecaseImpl
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object FeatureModule {
+    @Provides
+    fun provideGetCategoryUsecase():GetCategoryUseCase= GetCategoryUsecaseImpl()
+}
+
+@Module
+@InstallIn(ViewModelComponent::class)
+abstract class FeatureBindsModule {
+
+    @Binds
+    abstract fun bindsGetNewsSourcesUsecase(getNewsSourcesUsecaseImpl: GetNewsSourcesUsecaseImpl):GetNewsSourcesUsecase
+
+}
