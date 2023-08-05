cc4ca9236d7820336ccb9f075a52c7acb15058f6
billyandersonmatjik <billy.matjik@anteraja.id>
2023-08-06 02:13:37 +0700

    Change GetArticles Support Paging

--- app/src/main/java/com/bmatjik/myapplication/feature/usecase/GetArticlesByNewsSourceUsecase.kt
+++ app/src/main/java/com/bmatjik/myapplication/feature/usecase/GetArticlesByNewsSourceUsecase.kt
@@ -1,7 +1,9 @@
 package com.bmatjik.myapplication.feature.usecase
 
+import androidx.paging.PagingData
 import com.bmatjik.myapplication.feature.model.Article
+import kotlinx.coroutines.flow.Flow
 
 interface GetArticlesByNewsSourceUsecase {
-    suspend operator fun invoke(newsSource: String):Result<List<Article>>
+    suspend operator fun invoke(newsSource: String): Flow<PagingData<Article>>
 }
--- app/src/main/java/com/bmatjik/myapplication/feature/usecase/impl/GetArticlesByNewsSourceUsecaseImpl.kt
+++ app/src/main/java/com/bmatjik/myapplication/feature/usecase/impl/GetArticlesByNewsSourceUsecaseImpl.kt
@@ -1,22 +1,32 @@
 package com.bmatjik.myapplication.feature.usecase.impl
 
-import com.bmatjik.myapplication.core.repository.NewsRepository
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.bmatjik.myapplication.core.ArticlePagingSource
 import com.bmatjik.myapplication.feature.model.Article
 import com.bmatjik.myapplication.feature.usecase.GetArticlesByNewsSourceUsecase
 import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 @ViewModelScoped
-class GetArticlesByNewsSourceUsecaseImpl @Inject constructor(private val newsRepository: NewsRepository,private val coroutineDispatcher: CoroutineDispatcher):GetArticlesByNewsSourceUsecase {
-    override suspend fun invoke(newsSource: String): Result<List<Article>> {
-        return withContext(coroutineDispatcher){
-            newsRepository.getArticlesByNewsSource(newsSource).fold(onSuccess = {
-                 Result.success(it)
-            }, onFailure = {
-                 Result.failure(it)
-            })
-        }
+class GetArticlesByNewsSourceUsecaseImpl @Inject constructor(
+    private val articlePagingSource: ArticlePagingSource,
+    private val coroutineDispatcher: CoroutineDispatcher
+) : GetArticlesByNewsSourceUsecase {
+    override suspend fun invoke(newsSource: String): Flow<PagingData<Article>> {
+
+       return Pager(
+           // Configure how data should be loaded
+           PagingConfig(pageSize = 10,enablePlaceholders = true),
+           pagingSourceFactory = {
+               articlePagingSource.setNewsSource(newsSource)
+               articlePagingSource
+           }
+       ).flow.flowOn(coroutineDispatcher)
+
     }
 }
