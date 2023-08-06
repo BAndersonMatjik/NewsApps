589ec6af461be4f944a5da1a2bbac8eb1206234e
billyandersonmatjik <billy.matjik@anteraja.id>
2023-08-06 18:01:21 +0700

    Add minimal load newsSource

--- app/src/main/java/com/bmatjik/myapplication/common/Constants.kt
+++ app/src/main/java/com/bmatjik/myapplication/common/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
     val NewsCategory = listOf("business","general","health","science","sport","technology")
     const val FlagUrl = "https://flagcdn.com/w80/#img.png"
 
-    const val NEWS_SOURCE_MINIMAL_PAGE_SIZE = 5
+    const val NEWS_SOURCE_MINIMAL_PAGE_SIZE = 4
     const val NEWSAPI_STARTING_PAGE_INDEX = 1
     const val NEWSAPI_PAGE_SIZE = 10
 }
